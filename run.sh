85a78d5e6c487f2bbf195ceb7caa3123932e7c5c
Felipe Meyer <ffmeyer@gmail.com>
2022-02-28 10:34:03 -0300

    meu segundo commit

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
+#Exceutando com pasta de logs 
+#robot -d ./logs cadastro.robot
+#Executando tags especificas
+#robot -d ./logs -i inv_email cadastro.robot
+#exclusao de tags especificas (remove o inv_email e faz os demais do cadastro.robot)
+#robot -d ./logs -e inv_email cadastro.robot
+#passa o diretorio dos testes no comando (tests)
+#robot -d ./logs tests
+#executando tasks sem gerar nenhum tipo de relatorio (executor do limpador de banco de dados)
+#robot -l NONE -o NONE -r NONE tasks/Delorean.robot
 robot -l NONE -o NONE -r NONE tasks/Delorean.robot
 #robot -d ./logs -i i_pass tests/Signup.robot
 #obot -d ./logs tests/Signup.robot
